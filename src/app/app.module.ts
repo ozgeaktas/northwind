b202d724157e216812b3cfd443294fb8c1d43577
ozgeaktas <54955167+ozgeaktas@users.noreply.github.com>
2022-10-05 11:33:25 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './component/product/product.component';
+import { CategoryComponent } from './component/category/category.component';
+import { NaviComponent } from './component/navi/navi.component';
+import {TableModule} from 'primeng/table';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
