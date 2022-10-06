6ddffa51e6ed070b6ac153aae43227e29178458a
Özge Akdaş <54955167+ozgeaktas@users.noreply.github.com>
2022-10-06 11:40:53 +0300

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './component/product/product.component';
+import { CategoryComponent } from './component/category/category.component';
+import { NaviComponent } from './component/navi/navi.component';
+import {TableModule} from 'primeng/table';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent,
+    VatAddedPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
