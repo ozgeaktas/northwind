ebaddc91a218f487935aea0d16ab4fff32abc830
Özge Akdaş <54955167+ozgeaktas@users.noreply.github.com>
2022-10-06 11:44:05 +0300

    Add files via upload

---
+++ product.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import{HttpClient} from '@angular/common/http'
+import { VirtualTimeScheduler } from 'rxjs';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  products:Product[]=[];
+  dataLoaded=false;
+  filterText="";
+
+
+
+
+  constructor(private productService:ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.getProducts().subscribe(response =>{
+      this.products=response.data;
+      this.dataLoaded=true;
+    })
+
+  }
+
+}
